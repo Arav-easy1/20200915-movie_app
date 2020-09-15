e9ed3f4dd7e460ca709462fcf736f8b774d03ca3
EASY_ARAV <wg0418@gmail.com>
2020-09-16 00:05:29 +0900

    #2.0 Creating my first React Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Potato from "./Potato";
 
+// function Food(props) {
+//   console.log(props.fav);
+//   return <h1>I like Apple</h1>;
+// }
+// props object 내부에는 fav가 있음
+// props.fav = { fav }
+
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
+
+// JSX : JavaScript안의 HTML
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!!!!!!!!</h1>
+      <Potato />
+      {/* Food component에 fav라는 이름의 property(=Prop)를 kimchi라는 value로 줌. */}
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="hambergur" />
+      <Food fav="kebap" />
     </div>
   );
 }
