b1c77999ebe0bc01b43b6f017eb1eaa168ddfd23
EASY_ARAV <wg0418@gmail.com>
2020-09-16 20:19:31 +0900

    #2.3 map Recap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Potato from "./Potato";
 
 // function Food(props) {
 //   console.log(props.fav);
@@ -8,51 +7,58 @@ import Potato from "./Potato";
 // props object 내부에는 fav가 있음
 // props.fav = { fav }
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} />
-    </div>
-  );
-}
-
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg",
   },
   {
+    id: 2,
     name: "Samgyeopsal",
     image:
       "https://i1.wp.com/www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg?fit=900%2C600&ssl=1",
   },
   {
+    id: 3,
     name: "Bibimbap",
     image:
       "https://s23991.pcdn.co/wp-content/uploads/2017/06/bibimbap-recipe-fp.jpg",
   },
   {
+    id: 4,
     name: "Doncasu",
     image:
       "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
   },
   {
+    id: 5,
     name: "Kimbap",
     image:
       "https://www.thespruceeats.com/thmb/bXEyHCT-3algVEAy6GNDXnuCg3Y=/4288x2412/smart/filters:no_upscale()/kimbap-korean-sushi-rolls-2118795-Hero-5b7dbdd346e0fb00250718b8.jpg",
   },
 ];
 
+function renderFood(dish) {
+  return <Food key={dish.id} name={dish.name} picture={dish.image} />;
+}
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
 // JSX : JavaScript안의 HTML
 function App() {
   return (
     <div>
-      {/* Food component에 fav라는 이름의 property(=Prop)를 kimchi라는 value로 줌. */}
-      {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
-      ))}
+      {console.log(foodILike.map(renderFood))}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
