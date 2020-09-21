c4f8aaca153c319b3842d7a2683511145fd233b2
EASY_ARAV <wg0418@gmail.com>
2020-09-21 22:39:53 +0900

    4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 // function component는 function이고 뭔가를 return함. 그리고 screen에 표시 됨.
 // class component는 class임. react component로부터 확장되고 screen에 표시 됨.
@@ -9,9 +10,12 @@ class App extends React.Component {
     movies: [],
   };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    // Data Fatch 방법, 하지만 Axios라는 더 좋은 방법이 있다.
+    // fetch();
+    // 마치 fetch위의 작은 layer, 혹은 땅콩 주위의 멋진 초콜릿.
+    const movies = axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // axios.get은 항상 빠르지는 않음.
+    // javascript에게 componentDidMount함수가 끝날때까지 시간이 걸릴 수 있다고 말해야 됨. 그걸 기다려야 됨.
   }
   render() {
     const { isLoading } = this.state;
